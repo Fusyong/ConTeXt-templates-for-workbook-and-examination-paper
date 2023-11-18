6ddcc0e868d9aa06be9e2205d469aac425217c06
Fusyong <aahuaang@gmail.com>
2023-11-18 20:38:20 +0800

    田字格 done

--- envs/Pinyinge.lua
+++ envs/Pinyinge.lua
@@ -1,32 +1,44 @@
 -- https://wiki.contextgarden.net/String_manipulation
 function MP.tianzige(pinyin,characters) --lua.MP是给用户预留的命名空间，lua.mp则是系统自用的
-    local s = [[
-        pickup pencircle scaled 1pt ;
-        path p ; p := fullsquare scaled 1.3cm  shifted(%d(1.3cm),0cm);
-        draw p;
-        label.top(textext("\strut %s"), p) ;
-        label(textext("\itd %s"), p) ;
+    local mp_string = [[
+        u := 1.3cm;
+        right_y := %d(u);
+        
+        path square ; square := fullsquare scaled u shifted(right_y,0cm);
+        pickup pencircle scaled 0.5pt ;
+        draw square;
+        
+        path c[];
+        c1 := (-u/2, 0cm)--(u/2, 0cm);
+        c2 := (0cm, -u/2)--(0cm, u/2);
+        pickup pencircle scaled 0.3pt ;
+        draw c1 shifted(right_y,0cm) dashed evenly ;
+        draw c2 shifted(right_y,0cm) dashed evenly ;
+
+        label.top(textext("\strut %s"), square) ;
+        label(textext("\itd %s"), square) ;
     ]]
 
+    -- 分拆拼音音节，逐一存在表中
     local sep = "|"
-    local t={}
-    local p=""
+    local syllables={}
+    local syllable=""
     for c in string.utfcharacters(pinyin) do
         if c == sep then
-            table.insert(t,p)
-            p = ""
+            table.insert(syllables,syllable)
+            syllable = ""
         else
-            p = p..c
+            syllable = syllable..c
         end
     end
-    table.insert(t,p)
+    table.insert(syllables,syllable)
     
-    local cs = {}
+    local chineses = {}
     for c in string.utfcharacters(characters) do
-        table.insert(cs,c)
+        table.insert(chineses,c)
     end
-    for k,v in ipairs(t) do
-        mp.fprint(s,(k-1),v,cs[k])
+    for k,v in ipairs(syllables) do
+        mp.fprint(mp_string,(k-1),v,chineses[k])
     end
 end
 return MP.tianzige
